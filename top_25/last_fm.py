cd5a2e4cf3202f412fbc7d40451c583e4b7eeaf9
michaelkunc <mjkunc@gmail.com>
2017-01-23 14:19:38 -0800

    getting song ids

--- top_25/last_fm.py
+++ top_25/last_fm.py
@@ -12,29 +12,25 @@ r = requests.get(url, params=payload)
 
 data = r.json()['toptracks']['track']
 
+# this was originally a generator rather than a list comp. I think a generator
+# might be the right call
+song_artist = [(i['name'], i['artist']['name']) for i in data]
 
-song_artist = ((i['name'], i['artist']['name']) for i in data)
-
-for s in song_artist:
-    print(s[1])
 
 # get the Spotify song id for a given song.
 
 song_ids = []
 
 
-# def get_song_ids(song):
-# print(song)
-# song = requests.get(
-#     'https://api.spotify.com/v1/search?q={0}%20artist:{1}&type=track'.format(song.replace(' ', '%20'), artist.replace(' ', '%20')))
+def get_song_ids(song, artist):
+    song = requests.get(
+        'https://api.spotify.com/v1/search?q={0}%20artist:{1}&type=track'.format(song.replace(' ', '%20'), artist.replace(' ', '%20')))
 
-# song = song.json()
-
-# return song['tracks']['items'][0]['id']
+    return song.json()['tracks']['items'][0]['id']
 
 
-# for s in song_artist:
-#     print(get_song_ids(s[0]))
+for s in song_artist:
+    song_ids.append(get_song_ids(s[0], s[1]))
 
 
-# print(song_ids)
+print(song_ids)
